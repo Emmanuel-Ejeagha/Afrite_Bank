2686cca173fad7e2f4dc2dd96b35186aec0c2ffd
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-23 00:44:38 +0100

    Add: heHeaderBox props

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalBox from "@/components/TotalBalBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Emmy" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Manage your account and transactions with ease and efficiency."
+          />
+          <TotalBalBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={2500.5}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
