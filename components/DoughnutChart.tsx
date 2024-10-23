ce7ebf8778c6bad0b0989097b71faa1d7c5801b6
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-23 02:44:12 +0100

    Add: color to doughnut

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -11,12 +11,24 @@ const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
       {
         label: "Banks",
         data: [1250, 2434, 4350],
-        backgroundColor: ["#0747b6", "#2265d8", "#2f91fa"],
+        backgroundColor: ["#2265d8", "#EC8305", "#2f91fa"],
       },
     ],
     labels: ["Bank 1", "Bank 2", "Bank 3"],
   };
-  return <Doughnut data={data} />;
+  return (
+    <Doughnut
+      data={data}
+      options={{
+        cutout: "60%",
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      }}
+    />
+  );
 };
 
 export default DoughnutChart;
