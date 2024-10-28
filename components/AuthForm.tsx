4a531b60e638b7c00814f86ac3fc2b02028bc3f4
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-28 20:03:07 +0100

    Add: global error

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,13 +24,14 @@ import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import SignUp from "@/app/(auth)/sign-up/page";
 import { useRouter } from "next/navigation";
-import { logIn, signUp } from "@/lib/actions/user.actions";
+import { getLoggedInUser, logIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const formSchema = authFormSchema(type);
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -50,11 +51,11 @@ const AuthForm = ({ type }: { type: string }) => {
         setUser(newUser);
       }
       if (type === "login") {
-        // const response = await logIn({
-        //   email: data.email,
-        //   password: data.password,
-        // });
-        // if (response) router.push("/");
+        const response = await logIn({
+          email: data.email,
+          password: data.password,
+        });
+        if (response) router.push("/");
       }
     } catch (error) {
       console.log(error);
