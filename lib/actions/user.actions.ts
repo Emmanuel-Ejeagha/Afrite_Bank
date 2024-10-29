bf38e3c6319f5cc54135e343d10d042bb37f7121
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-29 09:53:34 +0100

    Implement logout endpoint

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,13 +1,16 @@
 "use server";
-
+import { parseStringify } from "./../utils";
 import { ID } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
-import { parseStringify } from "../utils";
 
-export const logIn = async () => {
+export const logIn = async ({ email, password }: signInProps) => {
   try {
     // Mutation / Database / make fetch
+    const { account } = await createAdminClient();
+    const response = await account.createEmailPasswordSession(email, password);
+
+    return parseStringify(response);
   } catch (error) {
     console.error("Error", error);
   }
@@ -42,8 +45,28 @@ export const signUp = async (userData: SignUpParams) => {
 export async function getLoggedInUser() {
   try {
     const { account } = await createSessionClient();
-    return await account.get();
+
+    const user = await account.get();
+    // console.log(user);
+    return parseStringify(user);
   } catch (error) {
+    console.log(error);
     return null;
   }
 }
+
+export const logoutAccount = async () => {
+  try {
+    const { account } = await createSessionClient();
+    const user = await account.get();
+    (await cookies()).delete("appwrite-session");
+    await account.deleteSession("current");
+    // provide feedback
+    console.log("Logout successfully");
+    return parseStringify(user);
+  } catch (error) {
+    console.error("Logout failed", error);
+    alert("Logout failed. Please try again.");
+    return null;
+  }
+};
