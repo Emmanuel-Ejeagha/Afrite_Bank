2fca5c6c349884963709736bc4fdf6ee74cd6a74
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-30 20:05:37 +0100

    Add: userData to signUp

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -48,7 +48,19 @@ const AuthForm = ({ type }: { type: string }) => {
     // Sign up with Appwrite & create plaid token
     try {
       if (type === "sign-up") {
-        const newUser = await signUp(data);
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBIrth: data.dateOfBirth!,
+          bvn: data.bvn!,
+          email: data.email,
+          password: data.password,
+        };
+        const newUser = await signUp(userData);
         setUser(newUser);
       }
       if (type === "login") {
