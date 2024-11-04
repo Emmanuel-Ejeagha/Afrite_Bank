a144de78a7e86c76a67d9657b19b87bec4a9fa2f
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-11-04 19:28:39 +0100

    Add: static elements to DoughnutChat

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,21 +1,26 @@
 "use client";
+
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
-import React from "react";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+  const accountNames = accounts.map((a) => a.name);
+  const balances = accounts.map((a) => a.currentBalance);
+
   const data = {
     datasets: [
       {
         label: "Banks",
-        data: [1250, 2434, 4350],
-        backgroundColor: ["#2265d8", "#EC8305", "#2f91fa"],
+        // data: balances,
+        data: [34750, 70045, 10350],
+        backgroundColor: ["#0747b6", "#EC8305", "#2f91fa"],
       },
     ],
     labels: ["Bank 1", "Bank 2", "Bank 3"],
   };
+
   return (
     <Doughnut
       data={data}
