ca18cabde270dd6c5c06c9ea6d2380841634b552
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-11-04 19:31:59 +0100

    Add: Mobile navBar

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,10 +1,5 @@
 "use client";
-// import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { sidebarLinks } from "@/constants";
-import { usePathname } from "next/navigation";
-import { cn } from "@/lib/utils";
+
 import {
   Sheet,
   SheetClose,
@@ -14,19 +9,24 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Footer from "./Footer";
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
 
   return (
-    <section className="w-full max-w-[264px]">
+    <section className="w-fulll max-w-[264px]">
       <Sheet>
         <SheetTrigger>
           <Image
             src="/icons/hamburger.svg"
-            height={30}
             width={30}
+            height={30}
             alt="menu"
             className="cursor-pointer"
           />
@@ -34,16 +34,16 @@ const MobileNav = ({ user }: MobileNavProps) => {
         <SheetContent side="left" className="border-none bg-white">
           <Link
             href="/"
-            className="flex cursor-pointer items-center gap-1 px-4"
+            className="cursor-pointer flex items-center gap-1 px-4"
           >
             <Image
               src="/icons/logo.svg"
               width={34}
               height={34}
-              alt="Afrite Bank"
+              alt="Horizon logo"
             />
             <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
-              Afrite Bank
+              Horizon
             </h1>
           </Link>
           <div className="mobilenav-sheet">
@@ -53,6 +53,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                   const isActive =
                     pathname === item.route ||
                     pathname.startsWith(`${item.route}/`);
+
                   return (
                     <SheetClose asChild key={item.route}>
                       <Link
@@ -62,17 +63,15 @@ const MobileNav = ({ user }: MobileNavProps) => {
                           "bg-bank-gradient": isActive,
                         })}
                       >
-                        <div className="relative size-6">
-                          <Image
-                            src={item.imgURL}
-                            alt={item.label}
-                            height={20}
-                            width={20}
-                            className={cn({
-                              "brightness-[3] invert-0": isActive,
-                            })}
-                          />
-                        </div>
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                          className={cn({
+                            "brightness-[3] invert-0": isActive,
+                          })}
+                        />
                         <p
                           className={cn("text-16 font-semibold text-black-2", {
                             "text-white": isActive,
@@ -87,6 +86,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                 USER
               </nav>
             </SheetClose>
+
             <Footer user={user} type="mobile" />
           </div>
         </SheetContent>
