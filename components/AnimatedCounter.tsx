d5dc9f0674d442a8d5c6d945e0ee285e462f5a05
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-11-04 15:51:14 +0100

    Add: static

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -5,7 +5,7 @@ import CountUp from "react-countup";
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
     <div className="w-full">
-      <CountUp duration={2} decimals={2} decimal="." prefix="₦" end={amount} />
+      <CountUp decimals={2} decimal="." prefix="₦" end={270500} />
     </div>
   );
 };
